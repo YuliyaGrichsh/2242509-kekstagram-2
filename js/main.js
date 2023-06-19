cdcfb04d2ef759445890725c02c213d00a440068
YuliyaGrichsh <moroz_yulia99@mail.ru>
2023-06-19 22:37:19 +0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+import {getMiniatures} from './api.js';
+import {insertPhotoMiniature} from './miniatures.js';
+import {showFileForm} from './form.js';
+import {showFilters} from './filters.js';
+
+//const descriptions = Array.from({length: PHOTOS_COUNT}, generateDescription);
+//insertPhotoMiniature(descriptions);
+const fileInput = document.querySelector('#upload-file');
+const preview = document.querySelector('.img-upload__preview').querySelector('img');
+
+getMiniatures()
+  .then((r) => {
+    insertPhotoMiniature(r);
+    showFilters(r);
+  });
+
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files[0];
+  if (!['png', 'jpg', 'jpeg'].some((suf) => file.name.toLowerCase().endsWith(suf))){
+    return;
+  }
+  preview.src = URL.createObjectURL(file);
+  showFileForm();
+});
